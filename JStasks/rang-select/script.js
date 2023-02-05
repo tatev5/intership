f3dbf0aa6184ac68f2e692411d9606e64bb7cd4a
Tatev <tatevs85@gmail.com>
2023-02-06 00:05:27 +0400

    website2 and range

--- JStasks/rang-select/script.js
+++ JStasks/rang-select/script.js
@@ -1,37 +1,45 @@
-for(let i =1;i<=20;i++){
-    let newDiv=document.createElement('div');
-    newDiv.setAttribute('id',`${i}`)
-    myNewdiv.appendChild(newDiv)
-    newDiv.style.backgroundColor='green';
-    newDiv.style.width='100px';
-    newDiv.style.height='100px';
-    newDiv.style.margin='10px';
-    myNewdiv.style.display='flex';
-    myNewdiv.style.flexWrap='wrap'
+for (let i = 1; i <= 20; i++) {
+  let newDiv = document.createElement('div');
+  newDiv.setAttribute('id', `${i}`)
+  myNewdiv.appendChild(newDiv)
+  newDiv.style.backgroundColor = 'green';
+  newDiv.style.width = '100px';
+  newDiv.style.height = '100px';
+  newDiv.style.margin = '10px';
+  myNewdiv.style.display = 'flex';
+  myNewdiv.style.flexWrap = 'wrap'
 }
 
-let divs=document.querySelectorAll('div');
+let divs = document.querySelectorAll('div');
 
-let arr=[]
-let  secondArr=[]
+let arr = []
+for (let i = 0; i < divs.length; i++) {
+  divs[i].onclick = function () {
 
-for(let i=0;i<divs.length;i++){
-    arr.push(divs[i].id)
-    
-    divs[i].onclick=function(e){
-  // e.target.style.backgroundColor='red'
-   secondArr.push(arr[i])
-   if(Number(secondArr[0]>secondArr[1])){
-    e.target.style.backgroundColor='red'
-    
-   }
+    arr.push(Number(divs[i].id))
+
+    divs[i].style.backgroundColor = 'red'
+    for (let j = arr[0]; j < arr[1]; j++) {
+      divs[j].style.backgroundColor = "red";
     }
+    if (arr[0] > arr[1]) {
+      for (let c = arr[1]; c < arr[0]; c++) {
+        divs[c].style.backgroundColor = "red";
+      }
+
+    }
+    if (arr[2]) {
+      for (let b = 0; b < divs.length; b++) {
+        divs[b].style.backgroundColor = "green";
+      }
+      if (arr[2]) {
+        divs[i].style.backgroundColor = "red";
+        arr = [];
+        arr.push(Number(divs[i].id));
+        
+       
+      }
+    }
+  }
 }
-    
-
-
-console.log(arr)
-
-console.log(secondArr)
-
 
